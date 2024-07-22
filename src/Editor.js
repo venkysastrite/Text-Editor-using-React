c3926b9ddf612d1438de447f611abcc63c237eb9
venkysastrite <161953002+venkysastrite@users.noreply.github.com>
2024-07-22 22:52:01 +0530

    Editor.js

--- src/Editor.js
+++ src/Editor.js
@@ -5,7 +5,7 @@ const Editor = ({text,setText})=>{
         <textarea id = "editor"
         value={text}
         onChange={(t)=>setText(t.target.value)}
-        style={{width:'600%', height:'300%'}}
+        style={{width:'300px', height:'300px'}}
         />
     );
 };
