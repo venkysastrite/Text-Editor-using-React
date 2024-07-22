4a6bb5734d55b8a5e7d0a53cdc7bd62403413bbc
venkysastrite <161953002+venkysastrite@users.noreply.github.com>
2024-07-22 22:56:28 +0530

    texteditor.js

--- src/texteditor.js
+++ src/texteditor.js
@@ -4,12 +4,14 @@ import FontWeight from './FontWeight';
 import Italic from './ToggleItalic';
 import Editor from './Editor';
 <html>
+    <head>
     <style>
     body{
     background-color: azure;
     margin:1%;
     }
     </style>
+        </head>
     </html>
 const TextEditor = () => {
     const [fontdata, setFontsData] = useState(null);
